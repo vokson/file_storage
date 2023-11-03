d2f4b434e68e2d67f43c194a7b7b64973895a6b3
Noskov Alexey <vokson@yandex.ru>
2023-11-03 11:22:02 +0300

    Combine migrations

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ up:
 	docker compose up -d --build
 migrate:
 	docker compose exec --workdir /backend/migrations backend python migrate.py
-reverse_migration:
-	docker compose exec --workdir /backend/migrations backend python revert.py
+revert:
+	docker compose exec --workdir /backend/migrations backend python migrate.py -r
 # pollute:
 # 	docker compose exec --workdir /opt/app/src/migrations auth python pollute.py && \
 # 	docker compose exec --workdir /opt/app/src/migrations storage python pollute.py
--- backend/migrations/migrate.py
+++ backend/migrations/migrate.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import logging
 import os
@@ -5,11 +6,13 @@ import sys
 from os import walk
 
 import asyncpg
+from asyncpg import Connection
 from asyncpg.exceptions import UndefinedTableError
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
-SQL_DIR = os.path.join(CURRENT_DIR, "sql", "forward")
+SQL_DIR_FORWARD = os.path.join(CURRENT_DIR, "sql", "forward")
+SQL_DIR_REVERSE = os.path.join(CURRENT_DIR, "sql", "reverse")
 
 sys.path.append(BASE_DIR)
 
@@ -18,14 +21,19 @@ from backend.core.config import db_dsl
 logger = logging.getLogger(__name__)
 
 
-async def get_migrations(conn):
+async def get_migrations(conn: Connection) -> list:
     try:
         return await conn.fetch("SELECT * FROM __migrations__;")
+
     except UndefinedTableError:
-        logger.warning("Table __migrations__ does not exist.")
+        logger.info("Creating __migrations__ table.")
+        await create_migrations(conn)
+        logger.warning("__migrations__ table has been created.")
+        return []
 
 
-async def create_migrations(conn):
+
+async def create_migrations(conn: Connection):
     await conn.execute(
         """
         CREATE TABLE __migrations__
@@ -38,19 +46,22 @@ async def create_migrations(conn):
     )
 
 
-async def apply_migration(conn, path, filename):
+async def apply_migration(conn: Connection, path: str, filename: str, is_new: bool):
+    migration_query = (
+        """
+        INSERT INTO __migrations__ (name, created)
+        VALUES ($1, CURRENT_TIMESTAMP);
+        """
+        if is_new
+        else "DELETE FROM __migrations__ WHERE name = $1;"
+    )
+
     with open(os.path.join(path, filename)) as f:
         query = f.read()
 
         try:
             await conn.execute(query)
-            await conn.execute(
-                """
-                INSERT INTO __migrations__ (name, created)
-                VALUES ($1, CURRENT_TIMESTAMP);
-                """,
-                filename,
-            )
+            await conn.execute(migration_query, filename)
             logger.info(f"{filename} - OK")
 
         except Exception as e:
@@ -62,31 +73,56 @@ def get_filenames(path):
     return next(walk(path), (None, None, []))[2]
 
 
-async def main():
+def get_args() -> tuple[bool, int]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--count", type=int, default=1)
+    parser.add_argument("-r", "--reverse", action="store_true")
+    args = parser.parse_args()
+    return args.reverse, args.count
 
-    conn = await asyncpg.connect(**db_dsl)
 
-    migrations = await get_migrations(conn)
-
-    #  Создаем таблицу __migrations__
-    if migrations is None:
-        logger.info("Creating __migrations__ table.")
-        await create_migrations(conn)
-        logger.warning("__migrations__ table has been created.")
-
+async def forward(conn: Connection, migrations: set[str]):
     #  Анализируем записи в __migrations__ и файлы на диске
-    migrations = {x["name"] for x in await get_migrations(conn)}
-    filenames = set(get_filenames(SQL_DIR))
-
+    filenames = set(get_filenames(SQL_DIR_FORWARD))
     not_applied_migrations = filenames - migrations
 
     #  Применяем миграции
     if len(not_applied_migrations) == 0:
         logger.info("Nothing to migrate.")
+        return
 
+    for filename in sorted(not_applied_migrations):
+        await apply_migration(conn, SQL_DIR_FORWARD, filename, True)
+
+
+async def revert(conn: Connection, migrations: set[str], count: int):
+    if len(migrations) ==0:
+        logger.info("No migrations !!")
+        return
+
+    #  Применяем миграции
+    logger.info(f"Reversing {count} migrations..")
+    for filename in sorted(migrations, reverse=True):
+        await apply_migration(conn, SQL_DIR_REVERSE, filename, False)
+
+        count -= 1
+        if count == 0:
+            break
+
+
+async def main():
+    reverse, count = get_args()
+    if reverse and count <= 0:
+        logger.info("Count of migrations to be reversed must be > 0")
+        return
+
+    conn = await asyncpg.connect(**db_dsl)
+    migrations = {x["name"] for x in await get_migrations(conn)}
+
+    if reverse:
+        await revert(conn, migrations, count)
     else:
-        for filename in sorted(not_applied_migrations):
-            await apply_migration(conn, SQL_DIR, filename)
+        await forward(conn, migrations)
 
     await conn.close()
 
