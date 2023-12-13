73ca9348ae86748e463bbf4458a806538810efcd
Alexey Noskov <vokson@yandex.ru>
2023-12-13 10:16:26 +0000

    Finish with backups of DB

--- backend/cron/db_backup.sh
+++ backend/cron/db_backup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin bash
+#!/bin/bash
 
 # Copy backups in ../backups directory. CHANGE IF NECESSARY.
 pg_dumpall -p ${DB__PORT} -c -U postgres | gzip > /backups/${SERVER_NAME}_`date +%d-%m-%Y"_"%H_%M_%S`.sql.gz
